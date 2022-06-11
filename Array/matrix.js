3169b0e43a65cb1d1e2f02d01f4b961c4fa0223f
swj <1061891980@qq.com>
2022-06-11 09:47:37 +0800

    零矩阵

--- Array/matrix.js
+++ Array/matrix.js
@@ -12,3 +12,26 @@ function rotate (matrix) {
         }
     }
 }
+
+// 面试题01.08 零矩阵
+function setZeroes(matrix) {
+    const m = matrix.length,n = matrix[0].length;
+    const row = new Array(m).fill(false);
+    const col = new Array(n).fill(false);
+
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
+            if(matrix[i][j] === 0){
+                row[i] = col[j] = true;
+            }
+        }
+    }
+
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j < n; j++){
+            if(row[i] || col[j]){
+                matrix[i][j] =  0;
+            }
+        }
+    }
+}
