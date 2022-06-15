5991ad635e16c671ab8f19486e883fab627fdec7
swj <1061891980@qq.com>
2022-06-15 08:58:20 +0800

    最长公共前缀的说明

--- String/最长公共前缀.js
+++ String/最长公共前缀.js
@@ -1,8 +1,10 @@
 function longestCommonPrefix(strs) {
     // 将字符串数组的第一项作为最长前缀 依次比较
     let res = strs[0] || '';
+    // 第一项作为前缀匹配 从第二项开始匹配循环
     for(let i = 1; i < strs.length;i++){
         while(strs[i].slice(0,res.length) !== res){
+            // 字符串中只要有一位不匹配 长度就减1
             res = res.slice(0,res.length - 1);
         }
     }
